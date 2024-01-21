34b071f2afb174782f95d76d78dfff53aa4276eb
MoharTry <mohartry@gmail.com>
2024-01-21 23:48:05 +0530

    Added functionalities

---
+++ components/file-upload.tsx
@@ -0,0 +1,11 @@
+"use client"
+
+const FileUpload = () => {
+    return (
+        <div>
+            File Upload Component
+        </div>
+      );
+}
+ 
+export default FileUpload;
--- components/modals/initial-modal.tsx
+++ components/modals/initial-modal.tsx
@@ -26,6 +26,7 @@ import {
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
 import { useRouter } from "next/navigation";
+import FileUpload from "../file-upload";
 
 const formSchema = z.object({
     name: z.string().min(1, {
@@ -79,7 +80,18 @@ export const InitialModal = () => {
                     <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
                         <div className="space-y-8 px-6">
                             <div className="flex items-center justify-center text-center">
-                                TODO: Image Upload
+                                <FormField
+                                    control={form.control}
+                                    name="imageUrl"
+                                    render={({ field }) => (
+                                        <FormItem>
+                                            <FormControl>
+                                                <FileUpload/>
+                                            </FormControl>
+                                        </FormItem>
+                                    )}
+                                
+                                />
                             </div>
                             <FormField
                                 control={form.control}
