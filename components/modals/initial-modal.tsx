2b1cca9b40f9d905b7bd28ef287f4b2ae1c36ed9
MoharTry <mohartry@gmail.com>
2024-01-21 23:16:53 +0530

    Added functionalities

--- components/modals/initial-modal.tsx
+++ components/modals/initial-modal.tsx
@@ -79,7 +79,7 @@ export const InitialModal = () => {
                     <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
                         <div className="space-y-8 px-6">
                             <div className="flex items-center justify-center text-center">
-                                
+                                TODO: Image Upload
                             </div>
                             <FormField
                                 control={form.control}
