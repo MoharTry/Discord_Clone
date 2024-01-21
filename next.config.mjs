e653adc648b5c7725e2bd093738c8b784ba9da27
MoharTry <mohartry@gmail.com>
2024-01-22 01:12:07 +0530

    Added Server Image Upload Function

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    images: {
+        domains: [
+            "utfs.io"
+        ]
+    }
+};
 
 export default nextConfig;
 
