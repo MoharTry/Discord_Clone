a0a4990534d3b8d37b20cbdb9731b82fc6902a10
MoharTry <mohartry@gmail.com>
2024-01-23 02:34:35 +0530

    Added Side Bar

--- components/modals/initial-modal.tsx
+++ components/modals/initial-modal.tsx
@@ -57,7 +57,7 @@ export const InitialModal = () => {
 
   const onSubmit = async (values: z.infer<typeof formSchema>) => {
     try {
-      await axios.post("/api/server", values);
+      await axios.post("/api/servers", values);
 
       form.reset();
       router.refresh();
