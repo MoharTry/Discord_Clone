eedcf1de7592c2bf89b82f0b8ad1e85cdb51d284
MoharTry <mohartry@gmail.com>
2024-02-10 20:41:32 +0530

    Member Setting Issue

--- hooks/use-modal-store.ts
+++ hooks/use-modal-store.ts
@@ -1,25 +1,28 @@
-import { Server } from "@prisma/client";
+import { Channel, ChannelType, Server } from "@prisma/client";
 import { create } from "zustand";
 
+export type ModalType = "createServer" | "invite" | "editServer" | "members" | "createChannel" | "leaveServer" | "deleteServer" | "deleteChannel" | "editChannel" | "messageFile" | "deleteMessage";
 
-export type ModalType = "createServer" | "invite" | "editServer" | "members";
-
-interface ModalData{
-    server?: Server
+interface ModalData {
+  server?: Server;
+  channel?: Channel;
+  channelType?: ChannelType;
+  apiUrl?: string;
+  query?: Record<string, any>;
 }
 
-interface ModalStore{
-    type: ModalType | null;
-    data: ModalData;
-    isOpen: boolean;
-    onOpen: (type: ModalType, data?: ModalData) => void;
-    onClose: () => void;
+interface ModalStore {
+  type: ModalType | null;
+  data: ModalData;
+  isOpen: boolean;
+  onOpen: (type: ModalType, data?: ModalData) => void;
+  onClose: () => void;
 }
 
 export const useModal = create<ModalStore>((set) => ({
-    type: null,
-    data: {},
-    isOpen: false,
-    onOpen: (type, data ={}) => set({ isOpen: true, type,data }),
-    onClose: () => set({ type: null, isOpen: false })
+  type: null,
+  data: {},
+  isOpen: false,
+  onOpen: (type, data = {}) => set({ isOpen: true, type, data }),
+  onClose: () => set({ type: null, isOpen: false })
 }));
