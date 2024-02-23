a8a504f42ea2baed04f32ac95ac50733910743d9
MoharTry <mohartry@gmail.com>
2024-02-24 00:08:56 +0530

    Member role

--- components/modals/members-modal.tsx
+++ components/modals/members-modal.tsx
@@ -7,21 +7,31 @@ import {
   DialogHeader,
   DialogTitle,
 } from "@/components/ui/dialog";
-
+import { MemberRole} from "@prisma/client";
 import { useModal } from "@/hooks/use-modal-store";
 import { ServerWithMembersWithProfiles } from "@/types";
 import { ScrollArea } from "@/components/ui/scroll-area";
 import { UserAvatar } from "@/components/user-avatar";
-import { ShieldAlert, ShieldCheck } from "lucide-react";
+import { MoreVertical, Shield, ShieldAlert, ShieldCheck, ShieldQuestion,Check } from "lucide-react";
 import { useRouter } from "next/navigation";
 import { useState } from "react";
-
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuPortal,
+  DropdownMenuSeparator,
+  DropdownMenuSub,
+  DropdownMenuSubContent,
+  DropdownMenuTrigger,
+  DropdownMenuSubTrigger,
+} from "@/components/ui/dropdown-menu";
 
 const roleIconMap = {
-  "GUEST": null,
-  "MODERATOR": <ShieldCheck className="h-4 w-4 ml-2 text-indigo-500" />,
-  "ADMIN": <ShieldAlert className="h-4 w-4 ml-2 text-rose-500" />
-}
+  GUEST: null,
+  MODERATOR: <ShieldCheck className="h-4 w-4 ml-2 text-indigo-500" />,
+  ADMIN: <ShieldAlert className="h-4 w-4 ml-2 text-rose-500" />,
+};
 
 export const MembersModal = () => {
   const router = useRouter();
@@ -50,19 +60,42 @@ export const MembersModal = () => {
                   {member.profile.name}
                   {roleIconMap[member.role]}
                 </div>
-                <p className="text-xs text-zinc-500">
-                  {member.profile.email}
-                </p>
+                <p className="text-xs text-zinc-500">{member.profile.email}</p>
               </div>
               {server.profileId !== member.profileId &&
-              loadingId !== member.id && (
-                <div>
-                  Actions!
-                </div>)}
+                loadingId !== member.id && (
+                  <div className="ml-auto">
+                    <DropdownMenu>
+                    <DropdownMenuTrigger>
+                      <MoreVertical className="h-4 w-4 text-zinc-500"/>
+                    </DropdownMenuTrigger>
+                    <DropdownMenuContent side="left">
+                      <DropdownMenuSub>
+                        <DropdownMenuSubTrigger className="flex items-center">
+                          <ShieldQuestion className="w-4 h-4 mr-2" />
+                          <span>Role</span>
+                        </DropdownMenuSubTrigger>
+                        <DropdownMenuPortal>
+                          <DropdownMenuSubContent>
+                            <DropdownMenuItem>
+                              <Shield className="w-4 h-4 mr-2"/>
+                              Guest
+                              {member.role === "GUEST" && (
+                                <Check className="w-4 h-4 ml-auto" 
+                                />
+                              )}
+                            </DropdownMenuItem>
+                          </DropdownMenuSubContent>
+                        </DropdownMenuPortal>
+                      </DropdownMenuSub>
+                    </DropdownMenuContent>
+                    </DropdownMenu>
+                  </div>
+                )}
             </div>
           ))}
         </ScrollArea>
       </DialogContent>
     </Dialog>
-  )
-}
+  );
+};
